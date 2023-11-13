ef201b0999b92e9e93c7f212349f380892eea464
Samantha Rivera Galo <samirivera1808@gmail.com>
2023-11-13 19:38:05 -0300

    desafio2

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
+const fs = require('fs');
+
 class ProductManager {
 
-    constructor() {
+    constructor(path) {
         this.products = []
         this.idProduct = 1
+        this.path = path
     }
 
-    addProduct(title, description, price, thumbnail, code, stock) {
+    async addProduct({ title, description, price, thumbnail, code, stock }) {
 
         //para que no se repita el codigo
         const repeatCode = this.products.find((product) => product.code === code)
@@ -35,42 +38,80 @@ class ProductManager {
         this.idProduct++;
         console.log(`El producto se agregó correctamente`)
 
+        //convierto el array en JSON
+        const productsString = JSON.stringify(this.products)
+        //escribir el producto en el archivo
+        await fs.writeFile(this.path, productsString, 'utf-8')
+
     }
 
-    getProducts() {
-        return this.products
+    async getProducts() {
+        //leer el archivo de productos y devolver todos los productos en formato de arreglo.
+        const productsString = await fs.readFile(this.path, 'utf-8')
+        const products = JSON.parse(productsString)
+        return products
     }
 
-    getProductById(idProduct) {
-        const foundProduct = this.products.find(
-            (prod) => prod.id === idProduct
-        );
-        if (!foundProduct) {
-            console.log("Error: Producto no encontrado");
-            return "Producto No Encontrado";
-        }
+    async getProductById(idProduct) {
+        //lea los productos y los pase a un array que es lo que hace la funcion getproducts
+        const products = await this.getProducts()
+        //luego encontrar el producto
+        const foundProduct = products.find((prod) => prod.id === idProduct)
         return foundProduct;
+    }
+
+    async updateProduct(idProduct, updatedFields) {
+
+        const products = await this.getProducts();
+
+        const indice = products.findIndex((prod) => prod.id === idProduct);
+
+        if (indice !== 1) {
+            // Actualizo
+            const updatedProduct = { ...products[indice], ...updatedFields };
+            products[indice] = updatedProduct;
+
+            // Escribo el producto actualizado
+            await fs.writeFile(path, JSON.stringify(products));
+            return updatedProduct;
+
+        } else {
+            console.log('Producto no encontrado')
+        }
+    }
+
+    async deleteProduct(idProduct) {
+        //primero lo leo
+        const products = await this.getProducts()
+
+        //luego busco que producto quiero eliminar
+        const foundProduct = products.find((prod) => prod.id === idProduct)
+
+        if (foundProduct) {
+            // elimino el producto
+            products.splice(products.indexOf(foundProduct), 1);
+            //convierto el array en json y luego lo escribo
+            const productsJson = JSON.stringify(this.products);
+            fs.writeFile('products.json', productsJson, 'utf-8');
+            console.log('El producto seleccionado fue eliminado')
+        } else {
+            console.log('Producto no encontrado')
+        }
     }
 }
 
-// pruebas
+//TEST
 
-const productManager = new ProductManager();
-//array vacio
-console.log(productManager.getProducts())
+const productManager = new ProductManager('./productos.json');
 
+productManager.getProducts();
 
-productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin img", "abc123", 25);
-productManager.addProduct("producto prueba 2", "Este es un producto prueba 2", 300, "sin img", "abc1234", 30);
-productManager.addProduct("producto prueba 3", "Este es un producto prueba 3", 100, "sin img", "abc12345", 40);
+productManager.addProduct({title: 'producto prueba' , description:'Este es un producto prueba', price:200, thumbnail:'Sin imagen' , code:'abc123', stock:25} )
 
-// codigo repetido
-productManager.addProduct("producto prueba", "Este es un producto prueba", 200, "sin img", "abc123", 25);
+productManager.getProducts();
 
+productManager.getProductById(1);
 
-console.log(productManager.getProductById(1));
-console.log(productManager.getProductById(2));
-console.log(productManager.getProductById(3));
-console.log(productManager.getProductById(4)) //cogido repetido
-// todos los productos agregados
-console.log(productManager.getProducts())
+productManager.updateProduct(2 ,  { title: 'prueba 2', description: 'Este es un producto prueba', price: 100, thumbnail: 'Sin imagen', code:'abc12345', stock: 41 })
+
+productManager.deleteProduct(1)
